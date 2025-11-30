5009ba24609414520b1dff926cf0e5c29ee49cc3
Infiza <inigo.infante@opendeusto.es>
2025-11-30 18:34:42 +0100

    Ecoembes en una carpteta

--- src/main/java/es/deusto/sd/ecoembes/external/IExternalRecyclingGateway.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.sd.ecoembes.external;
-
-import java.util.Optional;
-
-public interface IExternalRecyclingGateway {
-    boolean sendAssignment(ExternalAssignmentDTO dto);
-
-    Optional<ExternalPlantInfo> getPlantInfo(long plantId);
-}

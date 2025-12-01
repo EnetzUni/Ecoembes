f9f0195255a6be9852c5fa39ac170651568a341d
EnetzUni <150138458+EnetzUni@users.noreply.github.com>
2025-12-01 08:21:39 +0100

    Fix dumpster DAO

--- Ecoembes/src/main/java/es/deusto/sd/ecoembes/dao/DumpsterRepository.java
+++ Ecoembes/src/main/java/es/deusto/sd/ecoembes/dao/DumpsterRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import es.deusto.sd.ecoembes.entity.Dumpster;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface DumpsterRepository extends JpaRepository<Dumpster, Long> {
-    
 }

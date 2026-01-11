e0cbee55663d9fdc406d0c7fa51effc3b8d55f06
izaskun-s <izaskun.sebastian@opendeusto.es>
2026-01-11 17:19:47 +0100

    DumpsterDTO cambiado xq necesitamos datos para enseñar x pantalla

--- Ecoembes/src/main/java/es/deusto/sd/ecoembes/dto/DumpsterDTO.java
+++ Ecoembes/src/main/java/es/deusto/sd/ecoembes/dto/DumpsterDTO.java
@@ -5,12 +5,17 @@ public class DumpsterDTO {
     private long id;
     private String location;
     private float maxCapacity;
-
+    //miramos luego a ver si los quitamos
+    private int containerCount;    
+    private float fillLevel;       
+    
     public DumpsterDTO() {}
 
-    public DumpsterDTO(long id, String location, float maxCapacity) {
+    public DumpsterDTO(long id, String location, int containerCount, float fillLevel, float maxCapacity) {
         this.id = id;
         this.location = location;
+        this.containerCount = containerCount;
+        this.fillLevel = fillLevel;
         this.maxCapacity = maxCapacity;
     }
 
@@ -37,4 +42,24 @@ public class DumpsterDTO {
     public void setMaxCapacity(float maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
+    
+    
+    //el otro cambio. si al final todo va mal quitar estos dos
+    public float getFillLevel() { 
+    	return fillLevel; 
+    	}
+    
+    public void setFillLevel(float fillLevel) { 
+    	this.fillLevel = fillLevel; 
+    	}
+
+    public int getContainerCount() { 
+    	return containerCount; 
+    	}
+    
+    public void setContainerCount(int containerCount) { 
+    	this.containerCount = containerCount; 
+    	}
+
+   
 }

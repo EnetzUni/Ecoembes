f6da2c5eb15e044a7716d6c057996a7b336cbd8c
izaskun-s <izaskun.sebastian@opendeusto.es>
2026-01-11 17:15:42 +0100

    dao assignnment

--- Ecoembes/src/main/java/es/deusto/sd/ecoembes/dao/AssignmentRepository.java
+++ Ecoembes/src/main/java/es/deusto/sd/ecoembes/dao/AssignmentRepository.java
@@ -1,13 +1,14 @@
 package es.deusto.sd.ecoembes.dao;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import es.deusto.sd.ecoembes.entity.Assignment;
 import es.deusto.sd.ecoembes.entity.Dumpster;
+import java.util.List;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-    // ESTA LÍNEA ES OBLIGATORIA PARA QUE FUNCIONE EL SERVICE
-    List<Assignment> findByDumpster(Dumpster dumpster);
+    
+    List<Assignment> findByDumpstersContaining(Dumpster dumpster);
 }

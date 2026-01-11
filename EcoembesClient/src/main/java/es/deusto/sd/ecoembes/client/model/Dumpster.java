4793adb7d7a4edf0c48594f5992fd90b8dc77e9e
izaskun-s <izaskun.sebastian@opendeusto.es>
2026-01-11 17:35:36 +0100

    una vez mas, añadiendo lo q faltaba d dumpsters

--- EcoembesClient/src/main/java/es/deusto/sd/ecoembes/client/model/Dumpster.java
+++ EcoembesClient/src/main/java/es/deusto/sd/ecoembes/client/model/Dumpster.java
@@ -1,3 +1,9 @@
 package es.deusto.sd.ecoembes.client.model;
 
-public record Dumpster(long id, String location, float maxCapacity) {}
+public record Dumpster(
+    long id,
+    String location,
+    float maxCapacity,
+    int containerCount,
+    float fillLevel
+) {}
